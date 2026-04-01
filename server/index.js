57772ae8ae01b9427b373b18700deac78d1af845
Kashish Singh <kashish2392004@gmail.com>
2026-04-01 11:04:20 +0530

    new vercel url

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors(
   ({
   origin: [
-    'https://major-project-z191.vercel.app', 
+    'https://ielts-prep-final-dcpkckisq-kashish23092004s-projects.vercel.app/', 
     'http://localhost:5173'
   ],
   credentials: true
